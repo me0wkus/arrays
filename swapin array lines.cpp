ac3944e99a7f833a4000db6032ec810b6d2dbb1a
me0wkus <118027059+me0wkus@users.noreply.github.com>
2023-05-03 01:45:10 +0300

    Add files via upload

---
+++ swapin array lines.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+float ** creat(int& n, int& m)
+{
+	cout << "n = ";
+	cin >> n;
+	cout << "m = ";
+	cin >> m;
+	float **mas = new float*  [n];
+	for (int i = 0; i < n; ++i)
+		mas[i] = new float[m];
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < m; ++j)
+		{
+			cout << "mas[" << i << "][" << j << "] = ";
+			cin >> mas[i][j];
+		}
+	return mas;
+}
+int main()
+{
+	int n, m;
+	cout << "n = ";
+		cin >> n;
+		cout << "m = ";
+		cin >> m;
+	float** a = creat(n, m);
+	float* z;
+	for (int i = 0; i < (n / 2); ++i)
+	{
+		z = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = z;
+	}
+	for (int i = 0; i < n; ++i, cout << endl)
+		for (int j = 0; j < m; ++j)
+			cout << a[i][j] << "\t";
+	for (int i = 0; i < n; ++i)
+		delete[] a[i];
+	delete[] a;
+	return 0;
+}
