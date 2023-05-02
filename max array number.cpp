4f7233d58360420c70a717d007ce8977045b6551
me0wkus <118027059+me0wkus@users.noreply.github.com>
2023-05-03 00:47:44 +0300

    Add files via upload

---
+++ max array number.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int array = a[5][5], n, m;
+        cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+       for (int j = 0; j < m; j++)
+            cin >> array[i][j];
+    int max = array[0][0];
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            if (array[i][j] > max)
+            {
+                max = array[i][j];
+            }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "Max is = " << max;
+}
