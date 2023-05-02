51d06cf1f8ccb5880eb6ad0ef68ddcd0ff383ba1
me0wkus <118027059+me0wkus@users.noreply.github.com>
2023-05-03 01:06:21 +0300

    Add files via upload

---
+++ avg.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "russian");
+    int array[100][100], n, m;
+    double avg, sum = 0, count = 0;
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+        {
+            cin >> array[i][j];
+            if (array[i][j] % 2 == 0)
+            {
+                sum += array[i][j];
+                count++;
+            }
+        }
+    avg = sum / count;
+    cout << "Average is = " << avg;
+
+    return 0;
+}
