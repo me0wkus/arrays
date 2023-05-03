c10b5140a83a89841142cd6d231b2ca7168ef3de
me0wkus <118027059+me0wkus@users.noreply.github.com>
2023-05-03 14:45:05 +0300

    sum of main diagonal.cpp

--- sum of main diagonal.cpp
+++ sum of main diagonal.cpp
@@ -1,32 +1,36 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
-float **creat(int &n)
-{
-	cout << "n = ";
-	cin >> n;
-	float **mas = new float *[n];
-	for (int i = 0; i < n; ++i)
-		mas[i] = new float[n];
-	for (int i = 0; i < n; ++i)
-		for (int j = 0; j < n; ++i)
-		{
-			cout << "mas[" << i << "][" << j << "] = ";
-			cin >> mas[i][j];
-		}
 
-	return mas;
-}
-int main()
-{
-	int n;
-	float** a = creat(n);
-	float s = 0;
-	
-	for (int i = 0; i < n; i++)
-		s += a[i][i];
-	cout << "Sum of main diagonal = " << s;
-	for (int i = 0; i < n; i++)
-		delete[] a[i];
-	delete[] a;
-	return 0;
+int main() {
+    int n; 
+    double sum = 0; 
+
+    cout << "Enter yor matrix n: ";
+    cin >> n;
+
+    double** a = new double* [n]; 
+
+    for (int i = 0; i < n; i++) {
+        a[i] = new double[n];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << "Enter your matrix[" << i << "][" << j << "]: ";
+            cin >> a[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        sum += a[i][i];
+    }
+
+    cout << "Sum of main diagonal is " << sum << endl;
+
+    for (int i = 0; i < n; i++) {
+        delete[] a[i];
+    }
+    delete[] a;
+
+    return 0;
 }
